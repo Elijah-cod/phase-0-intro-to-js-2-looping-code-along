50b787a4ad45d8c856130cf0d05eca4ef7e484c2
Elijah <iamngunjiri@gmail.com>
2024-12-06 21:30:08 +0300

    Complete looping lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const writeCards = (strArr, eventName) => {
+    const newArr = []
+    for(let i=0; i<strArr.length; i++){
+        newArr.push(`Thank you, ${strArr[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return newArr
+}
+
+const countDown = (num) => {
+    while (num >= 0){
+        console.log(num)
+        num --
+    }
+}
